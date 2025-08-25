574b14a95c25e239d7df3a75a635026d748d5c14
Lucas2000-student <>
2025-08-25 20:14:12 -0300

    Contador!

--- Aula01.sql
+++ Aula01.sql
@@ -1,1 +1,19 @@
-SELECT * FROM ALUNO;
+SET SERVEROUTPUT ON;
+
+DECLARE
+--Variável
+v_contador NUMBER(10) := 1;
+v_fim NUMBER(10) := &qtd;
+
+BEGIN
+LOOP
+    DBMS_OUTPUT.PUT_LINE(v_contador);
+    v_contador := v_contador + 1;
+    EXIT WHEN v_contador > v_fim;
+END LOOP;
+
+EXCEPTION WHEN OTHERS THEN 
+    DBMS_OUTPUT.PUT_LINE('Ocorreu um erro:'||SQLERRM);
+
+END;
+/
