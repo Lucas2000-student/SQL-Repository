c5f1862aa4db8a7b43398745b790f3e91253e05c
Lucas2000-student <>
2025-08-25 20:44:51 -0300

    Fim da Aula 25/08

--- Aula01.sql
+++ Aula01.sql
@@ -17,3 +17,27 @@ EXCEPTION WHEN OTHERS THEN
 
 END;
 /
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+-- Variável
+v_rm ALUNO.rm%type := 1;
+v_nome ALUNO.nome%type;
+v_qtd_alunos number(10);
+
+BEGIN
+SELECT COUNT(*)INTO v_qtd_alunos FROM ALUNO;
+    LOOP
+        SELECT NOME INTO v_nome FROM ALUNO WHERE RM = v_rm;
+         DBMS_OUTPUT.PUT_LINE(v_rm || '° Aluno: ' || v_nome);
+         v_rm := v_rm + 1;
+         EXIT WHEN v_rm > v_qtd_alunos;
+    END LOOP;
+        
+EXCEPTION WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Ocorreu um erro:'||SQLERRM);
+
+END;
+/
+
